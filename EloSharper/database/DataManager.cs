ea56ff38b228174636dcd4acbf3c5dcb3615a502
Anchu <alee845@binghamton.edu>
2017-02-11 21:23:23 -0500

    Work

---
+++ EloSharper/database/DataManager.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+
+namespace EloSharper.database
+{
+	public class DataManager
+	{
+		private static string FileLocation = "./data/players.json";
+		private static DataManager _instance = new DataManager();
+
+		public static void Load()
+		{
+			if (!File.Exists(FileLocation))
+			{
+				throw new FileNotFoundException();
+				//File does not exist, handle it!
+			}
+			_instance = JsonConvert.DeserializeObject<DataManager>(File.ReadAllText(FileLocation));
+		}
+
+		public static void Save()
+		{
+			_instance.db.modified = (ulong)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+			using (var stream = new FileStream(FileLocation, FileMode.Create, FileAccess.Write, FileShare.None))
+			using (var writer = new StreamWriter(stream))
+				writer.Write(JsonConvert.SerializeObject(_instance, Formatting.Indented));
+		}
+
+		public class Database
+		{
+			[JsonProperty("title")]
+			public string title;
+			[JsonProperty("created")]
+			public ulong created;
+			[JsonProperty("modified")]
+			public ulong modified;
+			[JsonProperty("games")]
+			public List<Game> Games;
+			[JsonProperty("players")]
+			public List<Player> Players;
+		}
+
+		public class Player
+		{
+			[JsonProperty("name")]
+			public string name;
+			[JsonProperty("aliases")]
+			public List<string> aliases;
+			[JsonProperty("id")]
+			public string id;
+			[JsonProperty("rating")]
+			public ulong rating;
+			[JsonProperty("index")]
+			public int index;
+			[JsonProperty("totalgames")]
+			public int totalgames;
+			[JsonProperty("wins")]
+			public int wins;
+			[JsonProperty("draws")]
+			public int draws;
+		}
+
+		public class Game
+		{
+			[JsonProperty("index")]
+			public int index;
+			[JsonProperty("id")]
+			public string id;
+			[JsonProperty("datetime")]
+			public string DateTime;
+			[JsonProperty("players")]
+			public List<string> Players;
+			[JsonProperty("score")]
+			public string Score;
+		}
+
+		[JsonProperty("database")]
+		private Database db = new Database();
+		public static Database Data => _instance.db;
+	}
+}
---
+++ EloSharper/database/Database.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EloSharper.database
+{
+	public class Database
+	{
+		public Database()
+		{
+			DataManager.Load();
+		}
+
+		/// <summary>
+		/// Adds a new player.
+		/// </summary>
+		/// <returns><c>true</c>, if player was added, <c>false</c> otherwise.</returns>
+		/// <param name="name">Name.</param>
+		/// <param name="alias">Alias.</param>
+		public bool AddPlayer(string name, string alias)
+		{
+
+			if (FindPlayerByName(name) < 0)
+			{
+				var NewPlayer = new DataManager.Player();
+				NewPlayer.name = name;
+				NewPlayer.aliases = new List<string>(alias.Split(new char[0]));
+				NewPlayer.draws = 0;
+				NewPlayer.index = DataManager.Data.Players.Count;
+				NewPlayer.id = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+				NewPlayer.rating = 1000;
+				NewPlayer.totalgames = 0;
+				NewPlayer.wins = 0;
+				DataManager.Data.Players.Add(NewPlayer);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Finds the index of the player by name.
+		/// </summary>
+		/// <returns>The player index.</returns>
+		/// <param name="name">Name.</param>
+		private int FindPlayerByName(string name)
+		{
+			foreach (var plr in DataManager.Data.Players)
+			{
+				if (plr.name == name)
+				{
+					return plr.index;
+				}
+				foreach (string alias in plr.aliases)
+				{
+					if (alias == name)
+					{
+						return plr.index;
+					}
+				}
+			}
+			return -1;
+		}
+
+		/// <summary>
+		/// Finds the player index by identifier.
+		/// </summary>
+		/// <returns>The player index.</returns>
+		/// <param name="id">Identifier.</param>
+		private int FindPlayerByID(string id)
+		{
+			foreach (var plr in DataManager.Data.Players)
+			{
+				if (plr.id == id)
+				{
+					return plr.index;
+				}
+			}
+			return -1;
+		}
+
+	}
+}
